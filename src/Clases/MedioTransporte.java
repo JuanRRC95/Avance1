e5d6922cb59d1b131696e17f54bbc8bc426091a1
JuanR <juanricardorodriguez@ucundinamarca.edu.co>
2020-02-22 20:39:07 -0500

    Cambios en las clases para aplicar herencia con una nueva clase

---
+++ src/Clases/MedioTransporte.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Juan Rodriguez
+ */
+public abstract class MedioTransporte {
+    
+    private String placa;
+    private String Marca;
+    private int modelo;
+    private String nombreConductor;
+    private double cedula;
+    private double tarifaMinima;
+    
+    public MedioTransporte(String placa, String Marca, int modelo, String nombreConductor, double cedula, double tarifaMinima) {
+        this.placa = placa;
+        this.Marca = Marca;
+        this.modelo = modelo;
+        this.nombreConductor = nombreConductor;
+        this.cedula = cedula;
+        this.tarifaMinima = tarifaMinima;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getNombreConductor() {
+        return nombreConductor;
+    }
+
+    public void setNombreConductor(String nombreConductor) {
+        this.nombreConductor = nombreConductor;
+    }
+
+    public double getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(double cedula) {
+        this.cedula = cedula;
+    }
+
+    public double getTarifaMinima() {
+        return tarifaMinima;
+    }
+
+    public void setTarifaMinima(double tarifaMinima) {
+        this.tarifaMinima = tarifaMinima;
+    }
+    
+   
+    public String toString() {
+        return "Datos encontrados : \n"
+                + "Nombre Conductor: " + nombreConductor + "\n"
+                + "Marca :" + Marca + "\n"
+                + "Modelo :" + modelo + "\n"
+                + "Placa :" + placa + "\n"
+                + "Tarifa :" + tarifaMinima + "\n"
+                + "Cedula :" + cedula + "\n";
+    }
+    
+    
+    
+}
